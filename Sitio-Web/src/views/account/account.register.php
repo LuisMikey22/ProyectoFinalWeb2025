89eded4b52904c499936ba0984f19bb5ae18bf88
LuisMikey22 <llucatero_23@alu.uabcs.mx>
2025-11-26 01:31:32 -0700

    Campos removidos en formulario de registro

--- Sitio-Web/src/views/account/account.register.php
+++ Sitio-Web/src/views/account/account.register.php
@@ -22,51 +22,7 @@
                 <label for="description">Descripción de perfil</label>
                 <textarea name="description" placeholder="Descripción"></textarea>
             </div>
-
-            <div class="techniques-options-container">
-                <label for="fabric-techniques">Técnica de manejo de fibras favorita</label>
-
-                <select id="fabric-techniques" name="fabric-techniques" required>
-                    <optgroup label="Tejidos">
-                        <option value="loom">Telares</option>
-                        <option value="knitting">Tejido a aguja</option>
-                        <option value="crochet">Tejido a crochet</option>
-                        <option value="needle-felting">Fieltro a aguja</option>
-                        <option value="lace">Encaje (Lace)</option>
-                        <option value="embroidery">Bordado</option>
-                        <option value="macramé">Macramé</option>
-                        <option value="quilting">Edredón bordado (Quilting)</option> 
-                        <option value="patchwork">Manta bordada</option>
-                        <option value="Nålebinding">Tejido Escandinavo (Nålebinding)</option>  
-                    </optgroup>
-                </select> 
-            </div>
             
-
-            <div class="age-range-container">
-                <label for="Age">Edad</label>
-
-                <input id="age" list="ages" name="age" placeholder="Edad" required>
-
-                <datalist id="ages">
-                    <option value="15"></option>
-                    <option value="16"></option>
-                    <option value="17"></option>
-                    <option value="18"></option>
-                    <option value="19"></option>
-                    <option value="20"></option>
-                    <option value="21"></option>
-                    <option value="22"></option>
-                    <option value="23"></option>
-                    <option value="24"></option>
-                    <option value="25"></option>
-                    <option value="30"></option>
-                    <option value="40"></option>
-                    <option value="50"></option>
-                    <option value="60"></option>
-                    <option value="70"></option>
-                </datalist>
-            </div>
             <div class="action-container">
                 <a class="small-link" href="<?=BASE_PATH?>/login">¿Ya tienes una cuenta? Inicia sesión aquí</a>
                 <button class="create-account-button" type="submit">Registrarme</button>
