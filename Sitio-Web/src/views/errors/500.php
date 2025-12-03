461e3322e8c936a8cc7414081031cfa19826ef72
Axdiael <axtr_23alu.uabcs.mx>
2025-12-03 12:59:27 -0700

    error 500

---
+++ Sitio-Web/src/views/errors/500.php
@@ -0,0 +1,19 @@
+<div class="error-info">
+    <h1 class="text-6xl font-bold text-red-600 mb-4">
+        500
+    </h1>
+
+    <h2 class="error-title">
+        Error en el servidor
+    </h2>
+
+    <p class="error-desc">
+        Lo sentimos, hay un error interno en el servidor.
+        Estamos trabajando para solucionar el error, porfavor 
+        vuelva al inicio.
+    </p>
+
+    <a href="<?=BASE_PATH?>" class="action-button">
+        Volver al inicio
+    </a>
+</div>
